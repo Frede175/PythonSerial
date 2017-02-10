417047c67bd91b916e584ee6089b76a60495f5e5
Frderik Rosenberg <fsr1998@hotmail.com>
2017-02-10 19:26:35 +0100

    Sync

--- PythonSerial/Release/PythonSerial.cpp.d
+++ PythonSerial/Release/PythonSerial.cpp.d
@@ -1,22 +1,22 @@
-C:\Users\fsr19\AppData\Local\Temp\VMBuilds\PythonSerial\uno\Release\PythonSerial.cpp.o: \
- C:\Users\fsr19\AppData\Local\Temp\VMBuilds\PythonSerial\uno\Release\PythonSerial.cpp \
- C:\Programming\ Program\ Files\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
- C:\Programming\ Program\ Files\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
- C:\Programming\ Program\ Files\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
- C:\Programming\ Program\ Files\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
- C:\Programming\ Program\ Files\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
- C:\Programming\ Program\ Files\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
- C:\Programming\ Program\ Files\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
- C:\Programming\ Program\ Files\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
- C:\Programming\ Program\ Files\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
- C:\Programming\ Program\ Files\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
- C:\Programming\ Program\ Files\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
- C:\Programming\ Program\ Files\Arduino\libraries\Servo\src/Servo.h \
- C:\Programming\ Program\ Files\Arduino\libraries\Servo\src/avr/ServoTimers.h \
- C:\Programming\ Program\ Files\Arduino\hardware\arduino\avr\libraries\Wire\src/Wire.h \
- C:\Programming\ Program\ Files\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
- C:\Users\fsr19\Documents\Arduino\libraries\NewliquidCrystal/LiquidCrystal_I2C.h \
- C:\Programming\ Program\ Files\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
- C:\Users\fsr19\Documents\Arduino\libraries\NewliquidCrystal/I2CIO.h \
- C:\Users\fsr19\Documents\Arduino\libraries\NewliquidCrystal/LCD.h \
- C:\Programming\ Program\ Files\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h
+C:\Users\Frederik\AppData\Local\Temp\VMicroBuilds\PythonSerial\uno\PythonSerial.cpp.o: \
+ C:\Users\Frederik\AppData\Local\Temp\VMicroBuilds\PythonSerial\uno\PythonSerial.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\Servo\src/Servo.h \
+ C:\Program\ Files\ (x86)\Arduino\libraries\Servo\src/avr/ServoTimers.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire/Wire.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Users\Frederik\Documents\Arduino\libraries\LiquidCrystal/LiquidCrystal_I2C.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Users\Frederik\Documents\Arduino\libraries\LiquidCrystal/I2CIO.h \
+ C:\Users\Frederik\Documents\Arduino\libraries\LiquidCrystal/LCD.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h
